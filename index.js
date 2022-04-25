717e97cf00fa3e674b06b13981737a48e97c8d8a
Kevin Nielsen <knielsen24@gmail.com>
2022-04-25 10:56:22 -0700

    commit tree 9bdb25ca21642d2fab990d937437b8d3f873aff8

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!";
+  }
+  else if (feet > 400 && feet < 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+  return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const ans = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return ans;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity);
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case tip = "generous":
+      return "Thank you so much.";
+    case tip = "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
